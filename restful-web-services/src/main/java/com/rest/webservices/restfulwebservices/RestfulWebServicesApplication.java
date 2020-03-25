ac280771a76bf9fd13567edb0d1787bb29537acc
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-03-25 00:25:08 -0400

    Restful Webservices completed

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,12 @@
 package com.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,5 +14,11 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
-
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
